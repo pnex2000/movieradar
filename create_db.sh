84e39997f0fe2cd69322e27f8078ce900266c39c
Heikki Rauhala <heikki.rauhala@reaktor.fi>
2015-06-22 09:39:31 +0300

    Add execsql for nicer batch sql

--- create_db.sh
+++ create_db.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
-
+set -e
 dropdb --if-exists movieradar
 createdb --template=template0 --locale fi_FI.UTF-8 --encoding UTF-8 movieradar
 
-psql movieradar -f ./db/prepare_db.sql
-psql movieradar -f ./db/create_db.sql
+function execsql {
+    local db=$1
+    local file=$2
+    PGOPTIONS='--client-min-messages=warning' psql -X -q -1 -v ON_ERROR_STOP=1 --pset pager=off -d ${db} -f ${file}
+}
+
+execsql movieradar ./db/prepare_db.sql
+execsql movieradar ./db/create_db.sql
+
+echo "Done."
